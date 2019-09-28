4e223639cd3a3bbda9b356de8a1da9c0187e35f3
dialogicbuzz <55511525+dialogicbuzz@users.noreply.github.com>
2019-09-27 20:30:20 -0400

    Update deploy_ingress.sh
    remove cert generation command

--- ingress/deploy_ingress.sh
+++ ingress/deploy_ingress.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-#Generate a self-signed certificate
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout buzz-self-signed.key -out buzz-self-signed.crt -subj "/C=US/ST=New Jersey/L=Parsippany/O=Dialogic, Inc./OU=AFA/CN=buzz.dialogic.com"
-
 #Create a Secret for the certificate
 kubectl -n ingress-nginx create secret tls default-ssl-certificate --key  buzz-self-signed.key --cert buzz-self-signed.crt
 
