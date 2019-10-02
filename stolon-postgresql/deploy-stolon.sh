68c520529ece88df21643c7494816e2d7f6d853a
John Situ <john.situ@dialogic.com>
2019-10-02 11:36:03 -0400

    apply stolon namespace to stolonctl

--- stolon-postgresql/deploy-stolon.sh
+++ stolon-postgresql/deploy-stolon.sh
@@ -10,7 +10,7 @@ YAML_DIR="."
 kubectl apply -f "${YAML_DIR}"/stolon-namespace.yaml
 kubectl apply -f "${YAML_DIR}"/role.yaml
 kubectl apply -f "${YAML_DIR}"/role-binding.yaml
-kubectl run -i -t stolonctl --image=dialogicbuzz/stolon --restart=Never --rm -- /usr/local/bin/stolonctl --cluster-name=kube-stolon --store-backend=kubernetes --kube-resource-kind=configmap init
+kubectl -n stolon run -i -t stolonctl --image=dialogicbuzz/stolon --restart=Never --rm -- /usr/local/bin/stolonctl --cluster-name=kube-stolon --store-backend=kubernetes --kube-resource-kind=configmap init -y
 kubectl create -f "${YAML_DIR}"/secret.yaml
 kubectl create -f "${YAML_DIR}"/stolon-sentinel.yaml
 kubectl create -f "${YAML_DIR}"/stolon-keeper.yaml
