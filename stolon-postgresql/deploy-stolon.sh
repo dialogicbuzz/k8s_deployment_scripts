c294dfa9540acaff673e00262cdc1bcab44c3e63
John Situ <john.situ@dialogic.com>
2019-10-03 14:06:08 -0400

    apply stolon namespace

--- stolon-postgresql/deploy-stolon.sh
+++ stolon-postgresql/deploy-stolon.sh
@@ -4,15 +4,25 @@
 # The image must be available from the Docker registry specified by the image name.
 #
 
+# The deployment may be customised by defining the below vars before invoking this script
+#
+: "${STOLON_IMAGE:=sorintlab/stolon:master-pg10}"
+: "${STOLON_NAMESPACE:=stolon}"
+: "${YAML_DIR:=.}"
 
+if [ ${STOLON_NAMESPACE} != "default" ]; then
+  kubectl create namespace ${STOLON_NAMESPACE}
+fi
 
-YAML_DIR="."
-kubectl apply -f "${YAML_DIR}"/stolon-namespace.yaml
+sed -i "s/namespace:.*$/namespace: ${STOLON_NAMESPACE}/g" "${YAML_DIR}"/role.yaml
 kubectl apply -f "${YAML_DIR}"/role.yaml
+
+sed -i "s/namespace:.*$/namespace: ${STOLON_NAMESPACE}/g" "${YAML_DIR}"/role-binding.yaml
 kubectl apply -f "${YAML_DIR}"/role-binding.yaml
-kubectl -n stolon run -i -t stolonctl --image=dialogicbuzz/stolon --restart=Never --rm -- /usr/local/bin/stolonctl --cluster-name=kube-stolon --store-backend=kubernetes --kube-resource-kind=configmap init -y
-kubectl create -f "${YAML_DIR}"/secret.yaml
-kubectl create -f "${YAML_DIR}"/stolon-sentinel.yaml
-kubectl create -f "${YAML_DIR}"/stolon-keeper.yaml
-kubectl create -f "${YAML_DIR}"/stolon-proxy.yaml
-kubectl create -f "${YAML_DIR}"/stolon-proxy-service.yaml
+
+kubectl -n ${STOLON_NAMESPACE} run -i -t stolonctl --image=${STOLON_IMAGE} --restart=Never --rm -- /usr/local/bin/stolonctl --cluster-name=kube-stolon --store-backend=kubernetes --kube-resource-kind=configmap init -y
+kubectl -n ${STOLON_NAMESPACE} create -f "${YAML_DIR}"/secret.yaml
+kubectl -n ${STOLON_NAMESPACE} create -f "${YAML_DIR}"/stolon-sentinel.yaml
+kubectl -n ${STOLON_NAMESPACE} create -f "${YAML_DIR}"/stolon-keeper.yaml
+kubectl -n ${STOLON_NAMESPACE} create -f "${YAML_DIR}"/stolon-proxy.yaml
+kubectl -n ${STOLON_NAMESPACE} create -f "${YAML_DIR}"/stolon-proxy-service.yaml
